8488aa7dc0186eeec0892de5517fe7e547f69b70
tommyts94 <166877861+tommyts94@users.noreply.github.com>
2024-04-28 20:04:46 +0300

    Update configuration.nix

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -16,10 +16,10 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  nix.settings = {
-    substituters = ["https://hyprland.cachix.org"];
-    trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
-  };
+#  nix.settings = {
+#    substituters = ["https://hyprland.cachix.org"];
+#    trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+#  };
 
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
